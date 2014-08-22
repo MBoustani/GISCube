6bc50980aa02f04a53924a68f3bea11187c1dd09
Maziyar Boustani <boustani@jpl.nasa.gov>
2014-08-22 16:06:10 -0700

    main giscube django project folder

---
+++ giscube/giscube/config.py
@@ -0,0 +1,6 @@
+
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+BASE_DIR = '/Users/Mboustani/Documents/Code/web/project/GIS/giscube/giscube_app/static/'
+MEDIA_ROOT = '/Users/Mboustani/Documents/Code/web/project/GIS/giscube/giscube_app/static/'
+MEDIA_URL = 'uploaded_files/'
