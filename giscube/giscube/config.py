89233348c19b746096af028c8e0dd95259ba38d5
Maziyar Boustani <boustani@jpl.nasa.gov>
2014-08-22 16:23:09 -0700

    test

--- giscube/giscube/config.py
+++
@@ -1,6 +0,0 @@
-
-STATIC_ROOT = ''
-STATIC_URL = '/static/'
-BASE_DIR = '/Users/Mboustani/Documents/Code/web/project/GIS/giscube/giscube_app/static/'
-MEDIA_ROOT = '/Users/Mboustani/Documents/Code/web/project/GIS/giscube/giscube_app/static/'
-MEDIA_URL = 'uploaded_files/'
