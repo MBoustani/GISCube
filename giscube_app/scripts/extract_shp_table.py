7a04c0013c22282c8e6fa896b5910daf12dcc9f2
Maziyar Boustani <boustani@jpl.nasa.gov>
2014-09-08 15:43:47 -0700

    code that extracts attribute table from shapefile and save to csv file (from Geothon)

--- giscube_app/scripts/extract_shp_table.py
+++ giscube_app/scripts/extract_shp_table.py
@@ -47,12 +47,3 @@ def extract_shp_table(shp_file_name, csv_file_name):
             for i in range(num_field_col):
                 feautre_name.append(feautre.GetFieldAsString(i))
             writer.writerow(feautre_name)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Description of your program')
-    parser.add_argument('-shp','--shp', help='Description for foo argument', required=True)
-    parser.add_argument('-text_name','--text_name', help='Description for bar argument', required=True)
-    args = vars(parser.parse_args())
-    shp_file_name = args['shp']
-    csv_file_name = args['text_name']
-    extract_shp_table(shp_file_name, csv_file_name)
