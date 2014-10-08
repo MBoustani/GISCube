0adec86092c85e377bfc412f442e9e26899b427c
Maziyar Boustani <boustani@jpl.nasa.gov>
2014-10-08 10:41:57 -0700

    Limiting file formats can be uploaded and also removing BASE_DIR

--- giscube_app/views.py
+++ giscube_app/views.py
@@ -5,25 +5,40 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from giscube.config import BASE_DIR, MEDIA_ROOT, MEDIA_URL
+#import variables
+from giscube.config import MEDIA_ROOT, MEDIA_URL
+
+#import form and model to be able to upload files
 from giscube_app.models import Document
 from giscube_app.forms import DocumentForm
 
+#import GIS scripts
 from giscube_app.scripts.shp_name_info import run_shp_info
 from giscube_app.scripts.tif_name_info import run_tif_info
 from giscube_app.scripts.shp_to_kml import shp_to_kml
 from giscube_app.scripts.gtif_to_kml import gtif_to_kml
+from giscube_app.scripts.what_file import what_format
 
 
+#resource page
 def data_resource(request):
-    ALL_FILES = os.listdir(MEDIA_ROOT + MEDIA_URL)
-    UPLODED_FILES = [each for each in ALL_FILES if os.path.isfile(each)]
+    ALL_FILES = [each for each in os.listdir(MEDIA_ROOT+MEDIA_URL)]# if os.path.isfile(each)]
+    UPLODED_FILES = []
+    for each in ALL_FILES:
+        file_format = what_format(MEDIA_ROOT+MEDIA_URL+each)
+        if file_format == "ESRI Shapefile" and each.split(".")[-1]=="shp":
+            UPLODED_FILES.append(each)
+        elif file_format == "LIBKML":
+            UPLODED_FILES.append(each)
+        elif file_format == "GeoTIFF":
+            UPLODED_FILES.append(each)
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             newdoc = Document(docfile = request.FILES['docfile'])
             #get uploaded file name
             uploaded_file_name = newdoc.docfile.name
+            print uploaded_file_name
             file_extension = uploaded_file_name.split('.')[-1]
             if file_extension == "shp": #TODO: Use GDLA file format to recognize shapefiles, not with parsing
                 file_name_without_ext = uploaded_file_name.replace('.{0}'.format(file_extension),'')
@@ -49,7 +64,7 @@ def data_resource(request):
         context_instance=RequestContext(request)
     )
 
-
+#information page
 def data_information(request):
     os.chdir(MEDIA_ROOT + MEDIA_URL)
     shp_file_name = [each for each in glob.glob("*.shp") ]
@@ -66,7 +81,7 @@ def data_information(request):
     
     return render(request, 'data_information/index.html', context)
 
-
+#visualiser page
 def data_visualiser(request):
     os.chdir(MEDIA_ROOT + MEDIA_URL)
     shp_kmls_names = []
@@ -82,7 +97,7 @@ def data_visualiser(request):
     context = {'shp_kmls_names':shp_kmls_names, 'gtif_kmls_names':gtif_kmls_names}
     return render(request, 'data_visualiser/index.html', context)
 
-
+#tools page
 def tools(request):
     os.chdir(MEDIA_ROOT + MEDIA_URL)
     shp_file_name = [each for each in glob.glob("*.shp") ]
