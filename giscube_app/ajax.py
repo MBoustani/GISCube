75949ca37b4e0e02590191ea8369fe0ff0647e10
Maziyar Boustani <boustani@jpl.nasa.gov>
2014-10-08 10:40:33 -0700

    adding new tool as well as removing BASE_DIR

--- giscube_app/ajax.py
+++ giscube_app/ajax.py
@@ -1,6 +1,7 @@
 import os, shutil
 from dajaxice.decorators import dajaxice_register
-from giscube.config import BASE_DIR, MEDIA_ROOT, MEDIA_URL
+from giscube.config import MEDIA_ROOT, MEDIA_URL
+from scripts.extract_shp_table import extract_shp_table
 
 @dajaxice_register(method='GET')
 def remove_loaded_file(request, param):
@@ -15,6 +16,12 @@ def remove_loaded_file(request, param):
     
 @dajaxice_register(method='GET')
 def reproject_vector(request, selected_vector, selected_proj, vector_name):
-
     s = 'ogr2ogr -f "ESRI Shapefile" -s_srs EPSG:27700 -t_srs EPSG:4326 {0} {1}'.format(MEDIA_ROOT+MEDIA_URL+vector_name, MEDIA_ROOT+MEDIA_URL+selected_vector+'.shp')
     os.system(s)
+
+
+@dajaxice_register(method='GET')
+def extract_shp_table_text(request, selected_vector, text_name):
+    print MEDIA_ROOT+MEDIA_URL+selected_vector
+    print MEDIA_ROOT+MEDIA_URL+text_name
+    extract_shp_table(MEDIA_ROOT+MEDIA_URL+selected_vector+'.shp', MEDIA_ROOT+MEDIA_URL+text_name)
