e5e1c7d9d51f9693c7c64d645c4bc514f1b70f1a
Maziyar Boustani <boustani@jpl.nasa.gov>
2014-10-08 10:38:38 -0700

    set home page to resource page.

--- giscube_app/urls.py
+++ giscube_app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from giscube_app import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.data_resource, name='resource'),
     url(r'resource$', views.data_resource, name='resource'),
     url(r'information$', views.data_information, name = 'information'),
     url(r'visualiser$', views.data_visualiser, name = 'visualiser'),
