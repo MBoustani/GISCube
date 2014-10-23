10b258438e050d6907d463aed575b447481d464e
Maziyar Boustani <boustani@jpl.nasa.gov>
2014-10-23 15:54:38 -0700

    A conversion file that take care of all file conversions

---
+++ giscube_app/scripts/conversion.py
@@ -0,0 +1,66 @@
+try:
+    import gdal
+except ImportError:
+    from osgeo import gdal
+
+from osgeo import osr
+
+import numpy
+from numpy import ma
+
+
+def nc_to_gtif(latitudes, longitudes, values):
+    print "Creating GeoTIFF"
+    file_format = "GTiff"
+    driver = gdal.GetDriverByName(file_format)
+    
+    GEOTIFF_OUTPUT = "netCDF_in_geotiff.tif"
+
+    print "Getting values for making GeoTIFF"
+
+    try:
+        values = ma.array(values).data
+    except:
+        pass
+
+    print "Converting longitudes"
+    
+    for i in range(len(longitudes)):
+        print i
+        if longitudes[i] > 180:
+            longitudes[i] = longitudes[i] - 360
+
+    print "Calculating pixel size"
+
+    raster_x_size = longitudes.shape[0]
+    raster_y_size = latitudes.shape[0]
+
+    print "Rotating GeoTIFF"
+
+    values = numpy.rot90(values, 1)
+
+    print "Setting projection for GeoTIFF"
+
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(4326)
+    number_of_band = 1
+    raster_type = gdal.GDT_Float32
+
+    print "Creating raster dataset"
+
+    raster_dataset = driver.Create(GEOTIFF_OUTPUT,
+                                   raster_x_size,
+                                   raster_y_size,
+                                   number_of_band,
+                                   raster_type)
+
+    raster_dataset.GetRasterBand(1).WriteArray(values)
+
+    latitude_pixel_size = (abs(min(latitudes)) + abs(max(latitudes))) / len(latitudes)
+    longitude_pixel_size = (abs(min(longitudes)) + abs(max(longitudes))) / len(longitudes) 
+    
+    raster_dataset.SetGeoTransform((min(longitudes), latitude_pixel_size, 0, min(latitudes), 0, longitude_pixel_size))
+    raster_dataset.SetProjection(srs.ExportToWkt())
+
+    print "GeoTIFF Created"
+
