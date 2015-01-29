16cdd965a19f4b0d31eea389ce3c1b87f83f4e77
Maziyar Boustani <boustani@jpl.nasa.gov>
2015-01-28 16:52:15 -0800

    removing unnecessary file

--- giscube_app/scripts/shp_to_kml.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def shp_to_kml(shp_in):
-    kml_out = shp_in.replace('shp', 'kml')
-    string = 'ogr2ogr -f KML {0} {1}'.format(kml_out, shp_in)
-    os.system(string)
-    
-    return kml_out
