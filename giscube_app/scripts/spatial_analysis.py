b9b5f6318bb0ec7aae57afc6e703a0f57104e926
Maziyar Boustani <boustani@jpl.nasa.gov>
2015-04-08 13:05:27 -0700

    finding features from shapefile which contains given point

--- giscube_app/scripts/spatial_analysis.py
+++ giscube_app/scripts/spatial_analysis.py
@@ -37,3 +37,37 @@ def buffer_shapefile(selected_shp, buffer_shp_buffer_range, buffer_shp_out_name,
         return "Done."
     except:
         return "Someting went wrong"
+
+
+def find_point_inside_feature(selected_vector, point_inside_shapefile_lat, point_inside_shapefile_lon):
+    
+    in_shp_datasource = ogr.Open("{0}.shp".format(selected_vector))
+    in_layer = in_shp_datasource.GetLayerByIndex(0)
+    in_layer_defn = in_layer.GetLayerDefn()
+    num_field_col = in_layer_defn.GetFieldCount()
+
+    try:
+        point = ogr.Geometry(ogr.wkbPoint)
+        point.AddPoint(float(point_inside_shapefile_lon), float(point_inside_shapefile_lat))
+    except:
+        print "cannot create point geometry"
+
+    try:
+        field_name = []
+        for field in range(num_field_col):
+            field_name.append(in_layer_defn.GetFieldDefn(field).GetName())
+    except:
+        print "Cannot read feature attribute information"
+
+    features_number = in_layer.GetFeatureCount()
+    for i in range(features_number):
+        in_shp_feature = in_layer.GetFeature(i)
+        in_feature_geometry = in_shp_feature.GetGeometryRef()
+        if point.Within(in_feature_geometry):
+            field_count = in_shp_feature.GetFieldCount()
+            feature_info = ""
+            for field in range(field_count):
+                feature_info += "<h3>{0}: </h3> <p>{1}</p>".format(field_name[field], in_shp_feature.GetFieldAsString(field))
+            break
+
+    return feature_info
